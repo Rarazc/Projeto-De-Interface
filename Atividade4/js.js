81f43dc4a64823ecc721c3ea84d76ce4d35fcdc8
Rafael Braz <98630687+Rarazc@users.noreply.github.com>
2023-05-15 20:55:55 -0300

    bullshit

---
+++ Atividade4/js.js
@@ -0,0 +1,15 @@
+//Parametro inicial do nome da pagina
+var nome, x;
+nome = prompt("Qual o seu nome?");
+alert("Ola " + nome + " seu inutil, só continua")
+x = prompt("Qual o resultado de 5 x 5?");
+if(x != 25){
+    alert("Jumento!")
+}
+else{
+    alert("parabens sabe o minimo")
+}
+//botao inicial
+function clique_botao(){
+    alert("Só pra lembrar que você é um merda");
+}
